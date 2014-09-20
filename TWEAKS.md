60a9f2f99d775c21a8e966403ff821114b294e46
Ross Allan <rallanpcl@gmail.com>
2014-09-20 17:27:03 +0100

    First set of tweaks.

---
+++ TWEAKS.md
@@ -0,0 +1,14 @@
+# Customization
+* Tweaks which change vanilla behaviour will be optional, most will be on by default
+* Performance tweaks with no noticeable change in behaviour other than performance might not be optional
+
+# Implemented tweaks
+* Improved mob spawning, ability to set a multiplier for mob spawning
+* Reduced TileEntity default render range
+
+# TODO
+* Reduce log spam
+* Colored console output
+* Allow dimensions to unload in more cases
+* Disable spawn chunk loading
+* Asynchronous chunk loading
--- src/main/java/me/nallar/bettermobspawning/CoreMod.java
+++ src/main/java/me/nallar/bettermobspawning/CoreMod.java
@@ -1,13 +1,26 @@
 package me.nallar.bettermobspawning;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import me.nallar.javapatcher.mappings.MethodDescription;
 import me.nallar.modpatcher.ModPatcher;
 import net.minecraft.world.WorldServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.appender.FileAppender;
 
 import java.util.*;
 
 @IFMLLoadingPlugin.SortingIndex(1001) // Magic value, after deobf transformer.
 public class CoreMod implements IFMLLoadingPlugin {
+	public static final Logger log = LogManager.getLogger("nTweaks");
+
+	private static void logToFile() {
+		FileAppender fa = FileAppender.createAppender("./logs/Patcher.log", "false", "false", "PatcherAppender", "true", "true", "true", null, null, "false", null, null);
+		((org.apache.logging.log4j.core.Logger) LogManager.getLogger("ModPatcher")).addAppender(fa);
+		((org.apache.logging.log4j.core.Logger) LogManager.getLogger("JavaPatcher")).addAppender(fa);
+		((org.apache.logging.log4j.core.Logger) log).addAppender(fa);
+	}
+
 	public static float getMobSpawningMultiplier() {
 		return 1f;
 	}
@@ -31,9 +44,18 @@ public class CoreMod implements IFMLLoadingPlugin {
 		return ModPatcher.getSetupClass();
 	}
 
+	private void addPatch(String name, String description, boolean enabledByDefault) {
+		ModPatcher.getPatcher().readPatchesFromJsonInputStream(CoreMod.class.getResourceAsStream("/" + name + ".json"));
+	}
+
 	@Override
 	public void injectData(Map<String, Object> stringObjectMap) {
-		ModPatcher.getPatcher().readPatchesFromJsonInputStream(CoreMod.class.getResourceAsStream("/modpatcher.json"));
+		logToFile();
+		boolean isClient = true; // TODO Don't just invent a value
+		addPatch("mobSpawning", "Improved mob spawning algorithm which scales mob caps at night and has better performance.", true);
+		addPatch("tileEntityRenderRange", "Reduces the default tileEntity render range. Client only.", isClient);
+
+		log.info(ModPatcher.getPatcher().getMappings().map(MethodDescription.fromString("net.minecraft.tileentity.TileEntity", "getMaxRenderDistanceSquared")).toString());
 	}
 
 	@Override
