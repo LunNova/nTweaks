c613fb0671802cdff9a479014b9e6fb56ecca1c4
Ross Allan <rallanpcl@gmail.com>
2014-09-20 19:52:52 +0100

    Culling check order tweak

--- src/main/java/me/nallar/bettermobspawning/CoreMod.java
+++ src/main/java/me/nallar/bettermobspawning/CoreMod.java
@@ -1,6 +1,8 @@
 package me.nallar.bettermobspawning;
 
+import cpw.mods.fml.relauncher.FMLLaunchHandler;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.Side;
 import me.nallar.javapatcher.mappings.MethodDescription;
 import me.nallar.modpatcher.ModPatcher;
 import net.minecraft.world.WorldServer;
@@ -8,10 +10,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.appender.FileAppender;
 
+import java.io.*;
 import java.util.*;
 
 @IFMLLoadingPlugin.SortingIndex(1001) // Magic value, after deobf transformer.
 public class CoreMod implements IFMLLoadingPlugin {
+	private static boolean isClient = FMLLaunchHandler.side() == Side.CLIENT;
 	public static final Logger log = LogManager.getLogger("nTweaks");
 
 	private static void logToFile() {
@@ -45,15 +49,35 @@ public class CoreMod implements IFMLLoadingPlugin {
 	}
 
 	private void addPatch(String name, String description, boolean enabledByDefault) {
-		ModPatcher.getPatcher().readPatchesFromJsonInputStream(CoreMod.class.getResourceAsStream("/" + name + ".json"));
+		InputStream is = CoreMod.class.getResourceAsStream("/" + name + ".json");
+		if (is == null) {
+			ModPatcher.getPatcher().readPatchesFromXmlInputStream(CoreMod.class.getResourceAsStream("/" + name + ".xml"));
+		} else {
+			ModPatcher.getPatcher().readPatchesFromJsonInputStream(is);
+		}
+	}
+
+	private void addClientPatch(String name, String description, boolean enabledByDefault) {
+		if (isClient) {
+			addPatch(name, description, enabledByDefault);
+		}
+	}
+
+	private void addServerPatch(String name, String description, boolean enabledByDefault) {
+		if (!isClient) {
+			addPatch(name, description, enabledByDefault);
+		}
 	}
 
 	@Override
 	public void injectData(Map<String, Object> stringObjectMap) {
 		logToFile();
-		boolean isClient = true; // TODO Don't just invent a value
+
 		addPatch("mobSpawning", "Improved mob spawning algorithm which scales mob caps at night and has better performance.", true);
-		addPatch("tileEntityRenderRange", "Reduces the default tileEntity render range. Client only.", isClient);
+
+		addClientPatch("tileEntityRenderRange", "Reduces the default tileEntity render range", true);
+
+		addClientPatch("tileEntityCullingCheckOrder", "Check tile entity range culling before frustrum culling", false); // TODO determine if this actually helps
 
 		log.info(ModPatcher.getPatcher().getMappings().map(MethodDescription.fromString("net.minecraft.tileentity.TileEntity", "getMaxRenderDistanceSquared")).toString());
 	}
