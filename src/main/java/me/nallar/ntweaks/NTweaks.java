42d8c671bcd8ba1285cc695adcf3672dce8e7ab4
Ross Allan <rallanpcl@gmail.com>
2014-09-21 21:34:42 +0100

    Periodically check if any worlds with no players or loaded chunks need unloading

--- src/main/java/me/nallar/ntweaks/NTweaks.java
+++ src/main/java/me/nallar/ntweaks/NTweaks.java
@@ -1,9 +1,14 @@
 package me.nallar.ntweaks;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.WorldEvent;
 
@@ -14,10 +19,29 @@ public class NTweaks {
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register(this);
+		FMLCommonHandler.instance().bus().register(new UnloadTickHandler());
 	}
 
 	@SubscribeEvent (priority = EventPriority.LOWEST)
 	public void worldUnload(WorldEvent.Unload event) {
 		memoryLeakDetector.scheduleLeakCheck(event.world, "World " + event.world.provider.getDimensionName(), true);
+	}
+
+	public static class UnloadTickHandler {
+		private int counter = 0;
+
+		@SubscribeEvent
+		public void tick(TickEvent.ServerTickEvent tick) {
+			if (tick.phase == TickEvent.Phase.END && (++counter % 2000 == 100)) {
+				for (WorldServer worldServer : MinecraftServer.getServer().worldServers) {
+					int id = worldServer.provider.dimensionId;
+					if (id != 0
+							&& worldServer.getChunkProvider().getLoadedChunkCount() == 0
+							&& worldServer.playerEntities.isEmpty()) {
+						DimensionManager.unloadWorld(id);
+					}
+				}
+			}
+		}
 	}
 }
